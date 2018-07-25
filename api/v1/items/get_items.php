c23fbe8c53f8ef10698185c390b1dfeba43f3cb4
st-sathish <sathish.2009btech@gmail.com>
2018-07-25 10:41:56 +0530

    fixed param issue

--- api/v1/items/get_items.php
+++ api/v1/items/get_items.php
@@ -6,7 +6,9 @@ $item = new Item();
 if(!isset($params['limit'])) {
 	$params['limit'] = 10;
 }
-if(!isset($params['offset'])) {
+if(!isset($params['page'])) {
 	$params['offset'] = 0;
-} 
+} else {
+	$params['offset'] = $params['page'];
+}
 echo json_encode($item->get_items($params), JSON_UNESCAPED_SLASHES);
